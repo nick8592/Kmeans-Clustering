d6fd9ca1b42b39ccde06f0458597dccb602965d3
Nick Pai <nickybai8592@gmail.com>
2023-05-19 21:00:47 +0800

    add projection feature

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ from torchvision import datasets, transforms
 from tensor_type import Tensor
 from tqdm.auto import tqdm
 from sklearn.cluster import KMeans
-from sklearn.metrics import confusion_matrix, \
-    multilabel_confusion_matrix, precision_score, recall_score
+from sklearn.metrics import confusion_matrix, multilabel_confusion_matrix, precision_score, recall_score
 from sklearn.preprocessing import StandardScaler
-from skimage import measure
-from skimage.feature import hog
 from imagedominantcolor import DominantColor
+from utils import *
 
 def show_img(images: Tensor):
     for i in range(images.shape[0]):
@@ -70,164 +68,18 @@ def extract_features(images: Tensor):
         # Compute Dominate Color
         center1, center2, center3 = calculate_dominate_color(img)
 
+        # Compute Projection
+        row_non_zeros, column_non_zeros = calculate_projection(gray_arr)
+
         # Concatenate the features into a single array
         feature = np.concatenate([brightness, contours,
                                   h_hist, lines, circles, hog_features,
-                                  center1, center2, center3])
+                                  center1, center2, center3,
+                                  row_non_zeros, column_non_zeros])
         
         features.append(feature)
     features = np.array(features)
     return features
-
-def calculate_brightness(grayscale_image):
-    # Calculate histogram
-    hist, bins = np.histogram(grayscale_image.ravel(), 256, [0, 256])
-
-    pixels = sum(hist)
-    brightness = scale = len(hist)
-
-    for index in range(0, scale):
-        ratio = hist[index] / pixels
-        brightness += ratio * (-scale + index)
-
-    value = 1 if brightness == 255 else brightness / scale
-    value = round(value, 4)
-    return value
-
-def calculate_contours(grayscale_image, threshold:int = 128):
-    # Create a binary image by thresholding the grayscale image
-    binary = cv2.threshold(grayscale_image, threshold, 255, cv2.THRESH_BINARY)[1]
-
-    # Convert the grayscale image to a numpy array
-    img_arr = np.array(binary)
-
-    # Find contours in the image
-    contours = measure.find_contours(img_arr, 0.5)
-
-    # Calculate the number of contours
-    num_contours = len(contours)
-    return num_contours
-
-def calculate_euler_number(gray_arr):
-    euler_number = 0
-    for i in range(gray_arr.shape[0]-1):
-        for j in range(gray_arr.shape[1]-1):
-            a = gray_arr[i][j]
-            b = gray_arr[i][j+1]
-            c = gray_arr[i+1][j]
-            d = gray_arr[i+1][j+1]
-            if (a != b) or (a != c) or (a != d):
-                euler_number += 1
-    return euler_number
-
-def calculate_irregularity_ratio(gray_arr):
-    std_dev = np.std(gray_arr)
-    mean = np.mean(gray_arr)
-    irregularity_ratio = std_dev/mean
-    return irregularity_ratio
-
-def calculate_h_histogram(img_rgb):
-    # Convert RGB image to HSV color space
-    img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
-
-    # Extract the H channel from HSV image
-    h_channel = img_hsv[:, :, 0]
-    
-    # Calculate the histogram
-    hist, bins = np.histogram(h_channel, bins=180, range=[0, 180])
-    return hist
-
-def calculate_lines(gray_blur):
-    # Apply edge detection (can be skipped if the input image is already an edge map)
-    edges = cv2.Canny(cv2.convertScaleAbs(gray_blur), 50, 150, apertureSize=3)
-
-    # Apply Hough transform to detect lines
-    lines = cv2.HoughLines(edges, 1, np.pi/180, 100)
-
-    # Output the number of detected lines
-    if lines is not None:
-        return len(lines)
-    else:
-        return 0
-
-def calculate_circles(gray_blur):
-    # Detect circles using HoughCircles function
-    circles = cv2.HoughCircles(gray_blur, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=60, minRadius=0, maxRadius=150)
-
-    if circles is not None:
-        return len(circles)
-    else:
-        return 0
-
-def calculate_hog(gray_arr):
-    # Perform HOG feature extraction
-    hog_features = hog(gray_arr, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2))
-    return hog_features
-
-def calculate_standard_deviation(gray_arr):
-    # Calculate standard deviation
-    std_dev = np.std(gray_arr)
-    return std_dev
-
-def calculate_edge_histogram(img_rgb, gray_arr):
-    # Apply Canny edge detection
-    edges = cv2.Canny(cv2.convertScaleAbs(gray_arr), 100, 200)
-
-    # Dilate the edges to connect nearby edges
-    kernel = np.ones((5,5), np.uint8)
-    dilated_edges = cv2.dilate(edges, kernel, iterations=1)
-
-    # Find contours in the dilated edges
-    contours, hierarchy = cv2.findContours(dilated_edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Check that contours is not empty
-    if len(contours) > 0:
-        # Find the index of the largest contour
-        largest_contour_index = max(range(len(contours)), key=lambda i: cv2.contourArea(contours[i]))
-
-        # Find the index of the largest contour
-        largest_contour_index = max(range(len(contours)), key=lambda i: cv2.contourArea(contours[i]))
-
-        # Create a mask for the largest contour and fill it with 
-        mask = np.zeros_like(gray_arr)
-        cv2.drawContours(mask, [contours[largest_contour_index]], 0,  (255, 255, 255), -1)
-
-        # Calculate the area of the mask
-        mask_area = cv2.countNonZero(mask)
-
-        # Calculate the RGB histogram of the masked image
-        masked_img = cv2.bitwise_and(img_rgb, img_rgb, mask=mask)
-        color = ('b','g','r')
-        histograms = []
-        for i, col in enumerate(color):
-            hist = cv2.calcHist([masked_img], [i], mask, [256], [0, 256])
-            histograms.append(hist)
-    else:
-        # Handle the case where no contours were found
-        largest_contour_index = None
-        color = ('b','g','r')
-        histograms = []
-        for i, col in enumerate(color):
-            hist = cv2.calcHist([img_rgb], [i], None, [256], [0, 256])
-            histograms.append(hist)
-    r_hist = np.asarray(histograms[0]).reshape((256,))
-    g_hist = np.asarray(histograms[1]).reshape((256,))
-    b_hist = np.asarray(histograms[2]).reshape((256,))
-    rgb_hist_array = np.concatenate([r_hist, g_hist, b_hist])
-    return rgb_hist_array
-
-def calculate_dominate_color(img_rgb):
-    clt = KMeans(n_clusters=3, n_init='auto')
-    centers = clt.fit(img_rgb.reshape(-1, 3))
-    center1 = (centers.cluster_centers_[0]*255).astype(int)
-    center2 = (centers.cluster_centers_[1]*255).astype(int)
-    center3 = (centers.cluster_centers_[2]*255).astype(int)
-    return center1, center2, center3
-
-def calculate_projection(gray_arr):
-    # Apply edge detection (can be skipped if the input image is already an edge map)
-    edges = cv2.Canny(cv2.convertScaleAbs(gray_arr), 50, 150, apertureSize=3)
-    pass
 
 
 def get_precision(cm_arr):
