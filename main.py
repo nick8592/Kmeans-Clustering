5c64703fee40dbe7bda63f79607c5e5f45ecd54c
Nick Pai <nickybai8592@gmail.com>
2023-05-24 00:46:01 +0800

    micro -> macro

--- main.py
+++ main.py
@@ -209,8 +209,8 @@ for random_seed in tqdm(range(1000), desc='Find Best Random Seed'):
         # Perform k-means clustering
         kmeans = KMeans(n_clusters=num_clusters, random_state=random_state, n_init='auto').fit(scaled_train_features)
         predicted_labels = kmeans.predict(scaled_val_features)
-        pre = precision_score(val_labels, predicted_labels, average='micro', zero_division=1)
-        rec = recall_score(val_labels, predicted_labels, average='micro', zero_division=1)
+        pre = precision_score(val_labels, predicted_labels, average='macro', zero_division=1)
+        rec = recall_score(val_labels, predicted_labels, average='macro', zero_division=1)
         if pre + rec > best_precision + best_recall:
             best_precision = pre
             best_recall = rec
