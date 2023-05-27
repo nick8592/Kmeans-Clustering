6d3fd96c83cacf4b71a45fb0d58c09e3d098421f
nick8592 <nickybai8592@gmail.com>
2023-05-27 14:41:27 +0800

    replace row/col non_zero value with variance

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from sklearn.metrics import precision_score, recall_score
 from sklearn.preprocessing import StandardScaler
 from utils import *
 
+
 def show_img(images: Tensor):
     for i in range(images.shape[0]):
         img = transforms.ToPILImage()(images[i])
         img.show()
         input()
+
 
 # Define a function to extract features from an image
 def extract_features(images: Tensor):
@@ -23,7 +25,7 @@ def extract_features(images: Tensor):
         array = images[i].numpy()
 
         # Transpose the numpy array to match the format expected by OpenCV (H, W, C)
-        img = np.transpose(array, (1, 2, 0))*255
+        img = np.transpose(array, (1, 2, 0)) * 255
 
         # Convert the numpy array to an OpenCV image in grayscale format
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -98,7 +100,7 @@ def extract_features(images: Tensor):
         # rgb_std_dev = calculate_rgb_standard_deviation(img)
 
         # Compute Variance
-        # variance = calculate_variance(gray_arr)
+        variance = calculate_variance(gray_arr)
 
         # Compute Frequency domain Standard Deviation
         # freq_std_dev = calculate_freq_std(gray_arr)
@@ -110,36 +112,34 @@ def extract_features(images: Tensor):
         # feature_list = [brightness, std_dev, entropy, lines, perimeter,
         #                 row_non_zeros, column_non_zeros, non_zero_pixels,
         #                 h_hist, rgb_hist, a_hist, b_hist]
-        feature_list = [brightness, std_dev, entropy*10, lines, perimeter,
-                        row_non_zeros, column_non_zeros, non_zero_pixels,
-                        h_hist, rgb_hist, a_hist, b_hist]
-        # print(f"Brightness: {len(brightness)}")
-        # print(f"Standard Deviation: {len(std_dev)}")
-        # print(f"Entropy: {len(entropy)}")
-        # print(f"Lines: {len(lines)}")
-        # print(f"Perimeter: {len(perimeter)}")
-        # print(f"Row non-zeros: {len(row_non_zeros)}")
-        # print(f"Cloumn non-zeros: {len(column_non_zeros)}")
-        # print(f"Non-zeros: {len(non_zero_pixels)}")
-        # print(f"Hue Hist: {len(h_hist)}")
-        # print(f"RGB Hist: {len(rgb_hist)}")
-        # print(f"a Hist: {len(a_hist)}")
-        # print(f"b Hist: {len(b_hist)}")
-        # input()
+        feature_list = [
+            brightness,
+            std_dev,
+            entropy * 10,
+            variance,
+            lines,
+            perimeter,
+            non_zero_pixels,
+            h_hist,
+            rgb_hist,
+            a_hist,
+            b_hist,
+        ]
         feature = np.concatenate(feature_list)
-        
+
         features.append(feature)
     features = np.array(features)
     return features
 
-#-----------------------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------------------
 seed = 210
 # Find best random_state
 best_random_state_seed = None
 best_precision_seed = 0
 best_recall_seed = 0
 
-for random_seed in tqdm(range(1000), desc='Find Best Random Seed'):
+for random_seed in tqdm(range(1000), desc="Find Best Random Seed"):
     torch.manual_seed(random_seed)
 
     num_clusters = 10
@@ -147,53 +147,57 @@ for random_seed in tqdm(range(1000), desc='Find Best Random Seed'):
     # Define the transformation for the images
     # It's an option to transform image, you can either use this or design your transform function.
 
-    transform = transforms.Compose([
-        # transforms.Resize((224, 224)),  # Resize the images to a fixed size
-        transforms.ToTensor(),         # Convert the images to tensors
-        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize the images
-    ])
+    transform = transforms.Compose(
+        [
+            # transforms.Resize((224, 224)),  # Resize the images to a fixed size
+            transforms.ToTensor(),  # Convert the images to tensors
+            # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize the images
+        ]
+    )
 
     # Load the dataset
-    train_dataset = datasets.ImageFolder('dataset/train', transform=transform)
-    val_dataset = datasets.ImageFolder('dataset/test', transform=transform)
+    train_dataset = datasets.ImageFolder("dataset/train", transform=transform)
+    val_dataset = datasets.ImageFolder("dataset/test", transform=transform)
     assert train_dataset.class_to_idx == val_dataset.class_to_idx
     classes = train_dataset.class_to_idx
 
     # Create a data loader for the training set
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=10, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=10, shuffle=True
+    )
 
     # Create a data loader for the validation set
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=10, shuffle=True)
 
     # Set the device to use
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    device = 'cpu'
+    device = "cpu"
 
     # Get train images feature
     train_features = []
-    for images, labels in tqdm(train_loader, desc='Extract Train Image Features'):
+    for images, labels in tqdm(train_loader, desc="Extract Train Image Features"):
         images, labels = images.to(device), labels.to(device)
         features = extract_features(images)
         train_features.append(features)
     train_features = np.array(train_features)
-    train_image_num = train_features.shape[0]*train_features.shape[1]
+    train_image_num = train_features.shape[0] * train_features.shape[1]
     train_feature_num = train_features.shape[2]
     train_features = np.reshape(train_features, (train_image_num, train_feature_num))
 
     # Get val images feature
     val_features = []
     val_labels = []
-    for images, labels in tqdm(val_loader, desc='Extract Val Image Features'):
+    for images, labels in tqdm(val_loader, desc="Extract Val Image Features"):
         images, labels = images.to(device), labels.to(device)
         features = extract_features(images)
         val_features.append(features)
         val_labels.append(labels.numpy())
     val_features = np.array(val_features)
-    val_image_num = val_features.shape[0]*val_features.shape[1]
+    val_image_num = val_features.shape[0] * val_features.shape[1]
     val_feature_num = val_features.shape[2]
     val_features = np.reshape(val_features, (val_image_num, val_feature_num))
     val_labels = np.array(val_labels)
-    val_labels = np.reshape(val_labels, (val_image_num, ))
+    val_labels = np.reshape(val_labels, (val_image_num,))
 
     # Pre-Processing
     scaler = StandardScaler()
@@ -205,12 +209,18 @@ for random_seed in tqdm(range(1000), desc='Find Best Random Seed'):
     best_precision = 0
     best_recall = 0
 
-    for random_state in tqdm(range(100), desc='Find Best Random State'):
+    for random_state in tqdm(range(100), desc="Find Best Random State"):
         # Perform k-means clustering
-        kmeans = KMeans(n_clusters=num_clusters, random_state=random_state, n_init='auto').fit(scaled_train_features)
+        kmeans = KMeans(
+            n_clusters=num_clusters, random_state=random_state, n_init="auto"
+        ).fit(scaled_train_features)
         predicted_labels = kmeans.predict(scaled_val_features)
-        pre = precision_score(val_labels, predicted_labels, average='macro', zero_division=1)
-        rec = recall_score(val_labels, predicted_labels, average='macro', zero_division=1)
+        pre = precision_score(
+            val_labels, predicted_labels, average="macro", zero_division=1
+        )
+        rec = recall_score(
+            val_labels, predicted_labels, average="macro", zero_division=1
+        )
         if pre + rec > best_precision + best_recall:
             best_precision = pre
             best_recall = rec
@@ -218,36 +228,36 @@ for random_seed in tqdm(range(1000), desc='Find Best Random Seed'):
             best_predicted_labels = predicted_labels
 
     if best_precision + best_recall > best_precision_seed + best_recall_seed:
-            best_precision_seed = best_precision
-            best_recall_seed = best_recall
-            best_random_seed = random_seed
-            best_random_seed_state = best_random_state
-            best_predicted_labels_seed = best_predicted_labels
+        best_precision_seed = best_precision
+        best_recall_seed = best_recall
+        best_random_seed = random_seed
+        best_random_seed_state = best_random_state
+        best_predicted_labels_seed = best_predicted_labels
 
-    print('\n')
+    print("\n")
     print(f"Epoch({random_seed})")
     print(f"Best random seed: {best_random_seed}")
     print(f"Best random_state: {best_random_seed_state}")
     print(f"{'Class':13} |  {'Precision'}  |  {'Recall'}")
-    print('------------------------------------------')
+    print("------------------------------------------")
     print(f"{'Total':13} |     {best_precision_seed:.4f}  |  {best_recall_seed:.4f}")
-    print('\n')
+    print("\n")
 
 # Predict the clusters for the test images
 print(f"Val Labels: \n{val_labels}")
 print(f"Predicted Labels: \n{best_predicted_labels_seed}")
-print('==========================================================================')
+print("==========================================================================")
 print(f"Features Num: {scaled_train_features.shape[1]}")
 print(f"Best random seed: {best_random_seed}")
 print(f"Best random_state: {best_random_seed_state}")
-print('==========================================================================')
+print("==========================================================================")
 print(f"{'Class':13} |  {'Precision'}  |  {'Recall'}")
-print('------------------------------------------')
+print("------------------------------------------")
 for key, value in classes.items():
     val = [1 if n == value else 0 for n in val_labels]
     pred = [1 if n == value else 0 for n in best_predicted_labels_seed]
-    pre = precision_score(val, pred, average='binary', zero_division=1)
-    rec = recall_score(val, pred, average='binary', zero_division=1)
+    pre = precision_score(val, pred, average="binary", zero_division=1)
+    rec = recall_score(val, pred, average="binary", zero_division=1)
     print(f"{key:13} |     {pre:.4f}  |  {rec:.4f}")
-print('------------------------------------------')
+print("------------------------------------------")
 print(f"{'Total':13} |     {best_precision_seed:.4f}  |  {best_recall_seed:.4f}")
