e8b168e550368a1f4b40bf2a36cd86c84116f0c3
nick8592 <nickybai8592@gmail.com>
2023-05-28 01:59:32 +0800

    update main, zero_division should set to 0 instead of 1

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def extract_features(images: Tensor):
         # center1, center2, center3 = calculate_dominate_color(img)
 
         # Compute Projection
-        # row_non_zeros, column_non_zeros = calculate_projection(gray)
+        row_non_zeros, column_non_zeros = calculate_projection(gray)
 
         # Compute Entropy
         entropy = calculate_entropy(gray_arr)
@@ -113,9 +113,10 @@ def extract_features(images: Tensor):
             brightness,
             std_dev,
             entropy * 10,
-            variance,
             lines,
             perimeter,
+            row_non_zeros, 
+            column_non_zeros,
             non_zero_pixels,
             h_hist,
             rgb_hist,
@@ -130,7 +131,7 @@ def extract_features(images: Tensor):
 
 
 # -----------------------------------------------------------------------------------------------
-seed = 19
+seed = 138
 torch.manual_seed(seed)
 
 num_clusters = 10
@@ -205,9 +206,9 @@ for random_state in tqdm(range(100), desc="Find Best Random State"):
     ).fit(scaled_train_features)
     predicted_labels = kmeans.predict(scaled_val_features)
     pre = precision_score(
-        val_labels, predicted_labels, average="macro", zero_division=1
+        val_labels, predicted_labels, average="macro", zero_division=0
     )
-    rec = recall_score(val_labels, predicted_labels, average="macro", zero_division=1)
+    rec = recall_score(val_labels, predicted_labels, average="macro", zero_division=0)
     if pre + rec > best_precision + best_recall:
         best_precision = pre
         best_recall = rec
@@ -227,8 +228,8 @@ print("------------------------------------------")
 for key, value in classes.items():
     val = [1 if n == value else 0 for n in val_labels]
     pred = [1 if n == value else 0 for n in best_predicted_labels]
-    pre = precision_score(val, pred, average="binary", zero_division=1)
-    rec = recall_score(val, pred, average="binary", zero_division=1)
+    pre = precision_score(val, pred, average="binary", zero_division=0)
+    rec = recall_score(val, pred, average="binary", zero_division=0)
     print(f"{key:13} |     {pre:.4f}  |  {rec:.4f}")
 print("------------------------------------------")
 print(f"{'Total':13} |     {best_precision:.4f}  |  {best_recall:.4f}")
